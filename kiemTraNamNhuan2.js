b74b7accccb34789654fed1898841bcdf3678e54
Minh <minhdotcom@gmail.com>
2019-03-29 09:47:34 +0700

    done

--- kiemTraNamNhuan2.js
+++ kiemTraNamNhuan2.js
@@ -2,7 +2,7 @@ let year;
 year = Number(prompt('Nhap nam can kiem tra'));
 function leapYear(year)
 {
-    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    return (year % 400 == 0)|| ((year % 100 != 0) &&(year % 4 == 0))
 }
 if (leapYear(year))
     alert ('Nam nhuan');
